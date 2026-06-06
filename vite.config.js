43c472b84b2c934fd3543b09416e0989ebd94d41
thanjanbaburaj <thanjanbaburaj@gmail.com>
2026-06-06 18:21:44 +0400

    Add Vite configuration for React project

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/careeros-gcc/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  }
+})
